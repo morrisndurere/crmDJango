4825f2e954fe81345ce38553c1930583b80bb8f7
Morris Ndurere <morrisndurereofficial@gmail.com>
2022-03-24 12:27:51 +0300

    Initial Commit

---
+++ accounts/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE, null = True, blank = True)
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    profile_pic = models.ImageField(default='profilepic1.jpg', blank=True, null=True)
+    date_created = models.DateField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door')
+    )
+
+    name= models.CharField(max_length=200, null=True)
+    price= models.FloatField(null=True)
+    category= models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description= models.CharField(max_length=200, null=True, blank=True)
+    description= models.CharField(max_length=200, null=True, blank=True)
+    description= models.CharField(max_length=200, null=True, blank=True)
+    date_created= models.DateField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for Delivery', 'Out for Delivery'),
+        ('Delivered', 'Delivered')
+    )
+
+
+    product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
+    customer= models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
+    date_created= models.DateField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+    note = models.CharField(max_length=1000, null=True)
+
+    def __str__(self):
+        return self.product.name
+
